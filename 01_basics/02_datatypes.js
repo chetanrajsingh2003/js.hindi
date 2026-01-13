5b3930a27ec9a0a18bee679bbddf0c69965aca49
chetanrajsingh2003 <162435668+chetanrajsingh2003@users.noreply.github.com>
2026-01-13 21:35:05 +0000

    var commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3+3 )// we are using node js not browser
+
+console.log(3+3)
+// readiability should be high
+
+console.log("chetan")
+
+
+let name = "chetan"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//bollean => true/false
+//null=> standalone value
+//undifined =>undefined
+//symbol=>unique
+
+//object
+
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
