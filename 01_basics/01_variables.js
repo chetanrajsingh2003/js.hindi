0fd00a258b42bcc20c220338fc3c2f1d9fc6faf7
chetanrajsingh2003 <162435668+chetanrajsingh2003@users.noreply.github.com>
2026-01-13 21:05:19 +0000

    first

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "chetan@google.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+
+//accountId = 2 this is not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "22221111"
+accountCity = "delhi"
+
+console.log(accountId);
+/*
+prefer not to use var beacause
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity])
